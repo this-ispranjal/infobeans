aee06c666dc9e4dd0e64418078fbd10a59c78814
Pranjal Sharma <pranjalsharmaps5.com>
2024-11-06 22:39:39 +0530

    some-changes

--- java/p5.java
+++ java/p5.java
@@ -14,7 +14,6 @@ class test{
                 }
             }
             System.out.println();
-        }
-       
+        }   
     }
 }
--- java/p7.java
+++ java/p7.java
@@ -2,14 +2,14 @@ class test {
     public static void main(String args[]){
         int n ,l ,t,s, f,r;
 
-        n = 2345;
-        l = n%10;
-        t = (n/10) % 10;
-        s = (n/100) % 10;
-        f = (n/1000);
+        n = 2345%2000;
+        // l = n%10;
+        // t = (n/10) % 10;
+        // s = (n/100) % 10;
+        // f = (n/1000);
 
-        r = (l*1000+t*100+s*10+f);
-        System.out.println(r);
+        // r = (l*1000+t*100+s*10+f);
+        System.out.println(n);
         
     }
 }
