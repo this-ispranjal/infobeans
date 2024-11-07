ae4f32de27ed874a4194e6979e5169cdb740399e
Pranjal Sharma <pranjalsharmaps5.com>
2024-11-07 13:58:00 +0530

    core

---
+++ java/p10.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String args[]){
+        int n = 4852;
+
+        int t = n/2000;
+        int f = n%2000/500;
+        int tw = n%2000%500/200;
+        int oneh = ;
+    }
+}
--- java/p7.java
+++ java/p7.java
@@ -1,15 +1,20 @@
 class test {
     public static void main(String args[]){
-        int n ,l ,t,s, f,r;
+        // int n ,l ,t,s, f,r;
 
-        n = 2345%2000;
-        // l = n%10;
-        // t = (n/10) % 10;
-        // s = (n/100) % 10;
-        // f = (n/1000);
+        // n = 2345%2000;
+        // // l = n%10;
+        // // t = (n/10) % 10;
+        // // s = (n/100) % 10;
+        // // f = (n/1000);
 
-        // r = (l*1000+t*100+s*10+f);
-        System.out.println(n);
+        // // r = (l*1000+t*100+s*10+f);
+        // System.out.println(n);
         
+
+
+        int  n = 7373;
+
+        int t = 7;
     }
 }
