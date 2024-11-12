f335be709b405dfa866dda66b133dcfbafa7fea6
Pranjal Sharma <109729802+itspranjalsharma@users.noreply.github.com>
2024-11-12 23:37:38 +0530

    Update patter1.java

--- java/patter1.java
+++ java/patter1.java
@@ -1,13 +1,13 @@
 class test{
     public static void main(String args[]){
 
-        // 1.
-        // for(int i = 1; i<=3;i++){
-        //     for(int j = 1 ;j<=5;j++){
-        //         System.out.print(j+4);
-        //     }
-        //     System.out.println();
-        // }
+        1.
+        for(int i = 1; i<=3;i++){
+            for(int j = 1 ;j<=5;j++){
+                System.out.print(j+4);
+            }
+            System.out.println();
+        }
 
         //2. 
         // for(int i = 1; i<=3;i++){
