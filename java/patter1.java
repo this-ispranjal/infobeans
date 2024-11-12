3b971639cfbb8c5e03bb003c7c26306a4627fba0
Pranjal Sharma <109729802+itspranjalsharma@users.noreply.github.com>
2024-11-12 23:38:43 +0530

    uncommment

--- java/patter1.java
+++ java/patter1.java
@@ -42,12 +42,12 @@ class test{
 
         // 6. 
 
-        // for(int i = 1; i<=5;i++){
-        //     for(int j = 1 ;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i = 1; i<=5;i++){
+            for(int j = 1 ;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // 7. 
         // for(char i = 'A'; i<='E';i++){
