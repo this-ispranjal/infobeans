c53e072b4d8c41ad77755620cbd826fe8b829720
Pranjal Sharma <109729802+this-ispranjal@users.noreply.github.com>
2024-11-15 23:27:00 +0530

    Create praticeQues.java

---
+++ java/praticeQues.java
@@ -0,0 +1,8 @@
+  double a, b,c;
+      a = 3.0/0;
+      b = 0/4.0;
+      c=0/0.0;
+                            // outputs
+    System.out.println(a); // Infinity 
+      System.out.println(b); // 0.0
+      System.out.println(c); // NaN
