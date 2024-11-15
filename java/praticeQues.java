dabdc325e35052666c48a3875c37cea0dbe090d5
Pranjal Sharma <109729802+this-ispranjal@users.noreply.github.com>
2024-11-15 23:33:43 +0530

    2pratice.java

--- java/praticeQues.java
+++ java/praticeQues.java
@@ -1,4 +1,8 @@
-  double a, b,c;
+class test {
+    public static void main(String args[]){
+     
+      // 1. 
+      double a, b,c;
       a = 3.0/0;
       b = 0/4.0;
       c=0/0.0;
@@ -6,3 +10,18 @@
     System.out.println(a); // Infinity 
       System.out.println(b); // 0.0
       System.out.println(c); // NaN
+
+      // 2.
+       int x,y;
+      x = 5;
+      {
+    int y = 6;
+    System.out.print(x + " " + y);
+      }
+      System.out.println(x + " " + y);
+
+      // Output --> varible y not found 
+
+      // 3. 
+    }    
+}
