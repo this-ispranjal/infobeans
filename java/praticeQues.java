396133c86e3c566a471bbaa4de07ee96c6a159ab
Pranjal Sharma <109729802+this-ispranjal@users.noreply.github.com>
2024-11-15 23:35:26 +0530

    tercnariOperato

--- java/praticeQues.java
+++ java/praticeQues.java
@@ -22,6 +22,18 @@ class test {
 
       // Output --> varible y not found 
 
-      // 3. 
+      // 3. ternri operator 
+
+        // 6. 
+
+         int x , y = 1;
+      x = 10;
+      if (x != 10 && x / 0 == 0)
+          System.out.println(y);
+      else
+          System.out.println(++y);
+
+
+        
     }    
 }
