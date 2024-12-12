e9b5b5140639f272f4534b9bf77bf8819cfe1a3d
Pranjal Sharma <pranjalsharmaps5.com>
2024-12-12 16:22:54 +0530

    hello

---
+++ java/Function/func13dec.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+class f21 {
+
+    int x, y;
+
+    void add(int a, int b) {
+        x = a;
+        y = b;
+        System.out.println("sum = " + (a + b));
+    }
+
+    void sub() {
+        System.out.println("sum = " + (x - y));
+    }
+
+    void multi() {
+        System.out.println("sum = " + (x * y));
+    }
+
+    void divided() {
+        System.out.println("sum = " + (x / y));
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
